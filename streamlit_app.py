a3113ea280cf825003ebd38fa9e02fc5c229b02f
Cássio Campos <cassiocyberdefender@gmail.com>
2025-07-14 15:31:34 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,15 +2,15 @@ import streamlit as st
 from snowflake.snowpark.functions import col
 
 # Título do app
-st.title(" Smoothie!")
+st.title("Smoothie!")
 st.write("Choose the fruits you want in your custom Smoothie!")
 
 # Entrada de nome do pedido
 name_on_order = st.text_input("Name on Smoothie:")
-st.write('The name on your Smoothie Will be:', name_on_order)
+st.write('The name on your Smoothie will be:', name_on_order)
 
 # Obter a sessão ativa do Snowflake
-cnx = st.connection(''snowflake'')
+cnx = st.connection('snowflake')
 session = cnx.session()
 
 # Buscar a lista de frutas disponíveis
@@ -20,7 +20,7 @@ fruit_list = [row['FRUIT_NAME'] for row in my_dataframe.collect()]
 # Multiselect para escolher até 5 frutas
 ingredients_List = st.multiselect(
     'Choose up to 5 ingredients:',
-    my_dataframe,
+    fruit_list,
     max_selections=5
 )
 
@@ -35,8 +35,6 @@ if ingredients_List and name_on_order:
     time_to_insert = st.button('Submit Order')
 
     if time_to_insert:
-        # Opcional: mostrar SQL apenas quando o botão for clicado
-        # st.write("Preview SQL:", my_insert_stmt)
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!', icon="✅")
 
