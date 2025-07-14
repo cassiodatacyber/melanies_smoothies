80c270c3b82c7709d31ad63fad44ea262b6125ab
Cássio Campos <cassiocyberdefender@gmail.com>
2025-07-14 16:08:27 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,5 +41,5 @@ if ingredients_List and name_on_order:
  #New section to display smoothiefroot nutrition information
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon" )
-st.text(smoothiefroot_response)
+st.text(smoothiefroot_response.json())
 
